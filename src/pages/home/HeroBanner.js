4a998000a80b8db149c2db804cac4e65b9f0625c
Loren Cerio <cerioalex@gmail.com>
2024-05-09 14:39:26 +0800

    Update the Hero Banner Fixes #11

--- src/pages/home/HeroBanner.js
+++ src/pages/home/HeroBanner.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import useFetch from "../../hooks/useFetch";
+import { apiConfig } from "../../api/apiConfig";
 
 const HeroBanner = () => {
+  const { data, loading } = useFetch(`${apiConfig.BASE_URL}/movie/upcoming`);
+  const [heroImage, setHeroImage] = useState("");
+
+  useEffect(() => {
+    const fetchHeroImage = () => {
+      try {
+        if (data && data.results && data.results.length > 0) {
+          const movies = data.results;
+          const randomIndex = Math.floor(Math.random() * movies.length);
+          setHeroImage(movies[randomIndex].backdrop_path);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchHeroImage();
+  }, [data]);
+
+  const imageSource = heroImage ? `${apiConfig.imgOriginal(heroImage)}` : "";
+
   return (
-    <>
-      <div className="hero-banner">
-        <img
-          src="https://www.pluggedin.com/wp-content/uploads/2024/02/dune-part-two.jpg"
-          alt="Hero Banner"
-          className="hero-image"
-        />
-        <div className="hero-content">
-          <h1>Welcome.</h1>
-          <p>
-            Millions of movies, TV shows and people to discover. Explore now.
-          </p>
-        </div>
+    <div className="hero-banner">
+      {imageSource === null ||
+      imageSource === undefined ||
+      imageSource === "" ? (
+        <div className="no-image"></div>
+      ) : (
+        <img src={imageSource} alt="Hero Banner" className="hero-image" />
+      )}
+      <div className="hero-content">
+        <h1>Welcome.</h1>
+        <p>Millions of movies, TV shows and people to discover. Explore now.</p>
       </div>
-    </>
+    </div>
   );
 };
 
