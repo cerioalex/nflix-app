29d6c50c02c6b87512bc782bb1e8c8163b741031
Loren Cerio <cerioalex@gmail.com>
2024-05-23 12:32:45 +0800

    Update the genre selection option Fixes #17

--- src/pages/explore/Explore.js
+++ src/pages/explore/Explore.js
@@ -1,307 +1,152 @@
 import "../../styles/tvShows.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router";
-import { apiConfig } from "../../api/apiConfig";
+import { Typography } from "@mui/material";
+import { fetchDiscover, fetchMoviesByGenre } from "../../utils/api";
 import MovieBlock from "../../components/MovieBlock";
-import {
-  Typography,
-  CircularProgress,
-  IconButton,
-  FormControl,
-  InputLabel,
-  OutlinedInput,
-  InputAdornment,
-  MenuItem,
-  Select,
-  Box,
-  Chip,
-  Pagination,
-} from "@mui/material";
-import ClearIcon from "@mui/icons-material/Clear";
-
-let filters = {};
-
-const ITEM_HEIGHT = 50;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
+import GenreFilter from "../../components/GenreFilter";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Explore = () => {
-  const [data, setData] = useState(null);
-  const [pageNum, setPageNum] = useState(1);
+  const [data, setData] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
   const { mediaType } = useParams();
 
-  const [selectedGenreMovie, setSelectedGenreMovie] = useState([]);
-  const [selectedGenreTV, setSelectedGenreTV] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [genresMovie, setGenresMovie] = useState([]);
-  const [genresTV, setGenresTV] = useState([]);
+  const elementRef = useRef(null);
 
-  const fetchMovies = async () => {
-    try {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/discover/${mediaType}?api_key=${apiConfig.API_KEY}&page=${pageNum}`,
-        filters
-      );
-      if (!response.ok) {
-        throw new Error("Failed to fetch movies");
-      }
-      const data = await response.json();
-      setData(data.results);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error fetching movies:", error);
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    setPageNum(1); // Reset pageNum to 1 when mediaType changes
-  }, [mediaType]);
-
-  useEffect(() => {
-    fetchMovies();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [mediaType, pageNum]);
-
-  const handleChange = (event, value) => {
-    setPageNum(value);
-  };
-
-  const handleSelectChangeMovie = (event) => {
-    const { value } = event.target;
-    setSelectedGenreMovie(value);
-    // Fetch movies with selected genre
-  };
-
-  const handleSelectChangeTV = (event) => {
-    const { value } = event.target;
-    setSelectedGenreTV(value);
-    // Fetch TV shows with selected genre
-  };
-
-  // const handleSelectChange = (event) => {
-  //   const { value } = event.target;
-  //   setSelectedGenre(value);
-
-  //   if (value !== "") {
-  //     let genreId = value;
-  //     console.log("genreId");
-  //     console.log(genreId);
-
-  //     filters.with_genres = genreId;
-  //     console.log("filters.with_genres");
-  //     console.log(filters.with_genres);
-  //   } else {
-  //     delete filters.with_genres;
-  //   }
-
-  //   // fetchInitialData();
-  // };
-
-  const handleClear = () => {
-    if (mediaType === "movie") {
-      setSelectedGenreMovie([]);
-    } else {
-      setSelectedGenreTV([]);
-    }
-  };
-
-  useEffect(() => {
-    const fetchGenres = () => {
-      try {
-        const options = {
-          method: "GET",
-          headers: {
-            accept: "application/json",
-            Authorization: apiConfig.ACCESS_TOKEN,
-          },
-        };
-
-        fetch(
-          `https://api.themoviedb.org/3/genre/${mediaType}/list?language=en`,
-          options
-        )
-          .then((response) => response.json())
-          .then((response) => {
-            if (mediaType === "tv") {
-              setGenresTV(response.genres);
-            } else {
-              setGenresMovie(response.genres);
-            }
-          })
-          .catch((err) => console.error(err));
-      } catch (error) {
-        console.error("Error fetching genres:", error);
-      }
-    };
-    fetchGenres();
-  }, [mediaType]);
-
-  if (loading) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <CircularProgress color="inherit" />
-      </div>
-    );
-  }
+  const [selectedMovieGenres, setSelectedMovieGenres] = useState([]);
+  const [selectedTVGenres, setSelectedTVGenres] = useState([]);
 
   const selectedGenre =
-    mediaType === "tv" ? selectedGenreTV : selectedGenreMovie;
+    mediaType === "tv" ? selectedTVGenres : selectedMovieGenres;
 
-  const genres = mediaType === "tv" ? genresTV : genresMovie;
+  const handleGenreSelect = (genreId) => {
+    return mediaType === "tv"
+      ? setSelectedTVGenres(genreId)
+      : setSelectedMovieGenres(genreId);
+  };
 
-  const handleSelectChange =
-    mediaType === "tv" ? handleSelectChangeTV : handleSelectChangeMovie;
+  useEffect(() => {
+    const getMovies = async () => {
+      setIsLoading(true);
+      try {
+        let movies = [];
+        if (selectedGenre.length > 0) {
+          movies = await fetchMoviesByGenre(mediaType, selectedGenre, page);
+        } else {
+          movies = await fetchDiscover(mediaType, page);
+        }
 
-  const filteredMovies = selectedGenre
-    ? data.filter((movie) => movie.genre_ids.includes(parseInt(selectedGenre)))
-    : data;
+        if (movies.length === 0) {
+          setHasMore(false);
+        } else {
+          setData((prevData) => [...prevData, ...movies]);
+        }
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  console.log("Filtered Movies");
-  console.log(filteredMovies);
+    getMovies();
+  }, [mediaType, selectedGenre, page]);
 
-  if (!data || data.length === 0) {
-    return (
-      <div>
-        <Typography variant="h6" gutterBottom>
-          Error fetching data
-        </Typography>
-      </div>
-    );
-  }
+  // Reset state whenever mediaType changes
+  useEffect(() => {
+    setData([]);
+    setPage(1);
+    setHasMore(true);
+  }, [selectedGenre, mediaType]);
 
-  function getGenreNameById(id) {
-    const genre = genres.find((genre) => genre.id === id);
-    return genre ? genre.name : "";
-  }
+  // IntersectionObserver to trigger fetchMoreItems when the target is in view
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const firstEntry = entries[0];
+      if (firstEntry.isIntersecting && hasMore) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    });
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+    };
+  }, [hasMore]);
 
   return (
-    <div className="tvShow-wrapper">
-      <div className="row">
-        <div>
-          {mediaType === "tv" ? (
-            <Typography variant="h5" gutterBottom>
-              Explore TV Shows
-            </Typography>
+    <>
+      <div className="tvShow-wrapper">
+        <div className="explore-row">
+          <div className="explore-left">
+            {mediaType === "tv" ? (
+              <div style={{ textAlign: "center" }}>
+                <Typography variant="h5" gutterBottom>
+                  Explore TV Shows
+                </Typography>
+              </div>
+            ) : (
+              <div
+                style={{
+                  marginTop: 10,
+                }}
+              >
+                <Typography variant="h5" gutterBottom>
+                  Explore Movies
+                </Typography>
+              </div>
+            )}
+          </div>
+          <GenreFilter
+            mediaType={mediaType}
+            onGenreSelect={handleGenreSelect}
+          />
+        </div>
+
+        <div className="tvShow-container">
+          {data.length > 0 ? (
+            data.map((movie, index) => (
+              <div key={`${movie.id}-${index}`} className="tvShow-list">
+                <MovieBlock mediaType={mediaType} movie={movie} />
+              </div>
+            ))
           ) : (
-            <Typography variant="h5" gutterBottom>
-              Explore Movies
+            <Typography variant="h6" gutterBottom>
+              Sorry, results not found!
             </Typography>
           )}
         </div>
-
-        <FormControl
-          variant="filled"
-          sx={{
-            marginBottom: 2,
-            marginRight: 2,
-            minWidth: 250,
-            backgroundColor: "white",
-            borderRadius: 10,
-          }}
-        >
-          <InputLabel id="select-genre-label">Select Genre</InputLabel>
-          <Box>
-            <Select
-              multiple
-              labelId="genre"
-              id="genre"
-              value={selectedGenre}
-              onChange={handleSelectChange}
-              input={
-                <OutlinedInput
-                  id="select-multiple-chip"
-                  label="Chip"
-                  endAdornment={
-                    <InputAdornment position="end">
-                      {selectedGenre.length > 0 && (
-                        <IconButton
-                          onClick={handleClear}
-                          size="small"
-                          sx={{ mr: 2 }}
-                        >
-                          <ClearIcon />
-                        </IconButton>
-                      )}
-                    </InputAdornment>
-                  }
-                />
-              }
-              renderValue={(selected) => {
-                return (
-                  <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                    {selected.map((value) => (
-                      <Chip key={value} label={getGenreNameById(value)} />
-                    ))}
-                  </Box>
-                );
+        {hasMore && (
+          <div
+            ref={elementRef}
+            style={{
+              flexShrink: 0,
+              width: "100%",
+              display: "flex", // Use Flexbox for centering
+              justifyContent: "center", // Center horizontally
+              alignItems: "center", // Center vertically
+            }}
+          >
+            <div
+              style={{
+                display: "flex", // Use Flexbox for centering inside the inner div
+                justifyContent: "center", // Center horizontally inside the inner div
+                alignItems: "center", // Center vertically inside the inner div
               }}
-              MenuProps={MenuProps}
             >
-              <MenuItem value="">
-                <em>-----</em>
-              </MenuItem>
-              {genres.map((genre) => (
-                <MenuItem key={genre.id} value={genre.id}>
-                  {genre.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </Box>
-        </FormControl>
-      </div>
-
-      <div className="tvShow-container">
-        {filteredMovies.length > 0 ? (
-          filteredMovies.map((movie) => (
-            <div key={movie.id} className="tvShow-list">
-              <MovieBlock mediaType={mediaType} movie={movie} />
+              <CircularProgress />
             </div>
-          ))
-        ) : selectedGenre.length <= 0 ? (
-          data.map((movie) => (
-            <div key={movie.id} className="tvShow-list">
-              <MovieBlock mediaType={mediaType} movie={movie} />
-            </div>
-          ))
-        ) : (
-          <h1>No movies available </h1>
+          </div>
         )}
       </div>
-
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: 10,
-        }}
-      >
-        <Pagination
-          variant="outlined"
-          color="primary"
-          count={10}
-          page={pageNum}
-          onChange={handleChange}
-          size="large"
-          sx={{ backgroundColor: "pink" }}
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,38 +1,65 @@
-import axios from "axios";
-const BASE_URL = process.env.REACT_APP_API_URL;
-const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
+import { apiConfig } from "../api/apiConfig";
 
 const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: ACCESS_TOKEN,
+    Authorization: apiConfig.ACCESS_TOKEN,
   },
 };
 
-export const getMoviesData = async (url, params) => {
+const fetchGenres = async (mediaType) => {
   try {
-    const { data } = await axios.get(BASE_URL + url, options);
-    return data;
-  } catch (err) {
-    console.error(err);
-    return err;
+    const response = await fetch(
+      `${apiConfig.BASE_URL}/genre/${mediaType}/list?language=en`,
+      options
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch genres");
+    }
+    const data = await response.json();
+    return data.genres;
+  } catch (e) {
+    console.error(e);
+    return [];
   }
 };
 
-// const headers = {
-//   Authorization: ACCESS_TOKEN,
-// };
+const fetchMoviesByGenre = async (mediaType, genreIds, page) => {
+  try {
+    const genreString = genreIds.join(",");
+    console.log(genreString);
+    const response = await fetch(
+      `${apiConfig.BASE_URL}/discover/${mediaType}?api_key=${
+        apiConfig.API_KEY
+      }&with_genres=${genreString}&page=${page + 1}`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch movies by genre");
+    }
+    const data = await response.json();
+    return data.results;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+};
 
-// export const getMoviesData = async (url, params) => {
-//   try {
-//     const { data } = await axios.get(BASE_URL + url, {
-//       headers,
-//       params,
-//     });
-//     return data;
-//   } catch (err) {
-//     console.error(err);
-//     return err;
-//   }
-// };
+const fetchDiscover = async (mediaType, page) => {
+  try {
+    const response = await fetch(
+      `${apiConfig.BASE_URL}/discover/${mediaType}?page=${page + 1}`,
+      options
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch movies by genre");
+    }
+    const data = await response.json();
+    return data.results;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+};
+
+export { fetchGenres, fetchMoviesByGenre, fetchDiscover };
